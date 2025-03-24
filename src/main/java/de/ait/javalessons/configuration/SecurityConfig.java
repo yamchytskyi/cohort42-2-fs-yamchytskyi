7788730e1e121693af3be28f3b1a92f2b8ffd5d1
yamchytskyi <wk.vitalii@gmail.com>
2025-03-24 19:38:31 +0100

    Jdbc security

--- src/main/java/de/ait/javalessons/configuration/SecurityConfig.java
+++ src/main/java/de/ait/javalessons/configuration/SecurityConfig.java
@@ -1,10 +1,12 @@
 package de.ait.javalessons.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
@@ -12,6 +14,8 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
@@ -20,41 +24,33 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(JdbcUserDetailsManager jdbcUserDetailsManager, PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        daoAuthenticationProvider.setUserDetailsService(jdbcUserDetailsManager);
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, DaoAuthenticationProvider daoAuthenticationProvider) throws Exception {
         http
-                .authenticationProvider(daoAuthenticationProvider(
-                        .
-                ))
+                .authenticationProvider(daoAuthenticationProvider)
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/admin/**").hasRole(("ADMIN"))
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/user/**").hasRole("USER")
-                        .requestMatchers("/", "/public/**", "h2-console/**").permitAll()
+                        .requestMatchers("/", "/login/**", "/public/**", "/h2-console/**", "/swagger-ui/**").permitAll()
                         .anyRequest().authenticated()
                 )
+                .headers(headers -> headers.frameOptions().sameOrigin())
                 .formLogin(Customizer.withDefaults())
-                .httpBasic(Customizer.withDefaults())
                 .logout(logout -> logout.permitAll());
 
-        http.headers(headers -> headers.frameOptions().disable());
-
         return http.build();
-    }
-
-    @Bean
-    public static void initializeAuthenticationManagerBuilder(AuthenticationManagerBuilder authenticationManagerBuilder,
-                                                              DataSource dataSource) throws Exception{
-        authenticationManagerBuilder.jdbcAuthentication().dataSource(dataSource);
-
-    }
-
-    @Bean
-    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
-        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-        return  jdbcUserDetailsManager;
-    }
-
-    @Bean
-    public DaoAuthenticationProvider daoAuthenticationProvider(PasswordEncoder passwordEncoder) {
-        DaoAuthenticationProvider daoAuthenticationProvider = new
     }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users
+
+(
+
+    username VARCHAR(50)  NOT NULL PRIMARY KEY,
+
+    password VARCHAR(100) NOT NULL,
+
+    enabled  BOOLEAN      NOT NULL
+
+);
+
+CREATE TABLE authorities
+
+(
+
+    username  VARCHAR(50) NOT NULL,
+
+    authority VARCHAR(50) NOT NULL,
+
+    CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
+
+);
+
+INSERT INTO users (username, password, enabled)
+
+VALUES ('admin', '$2a$10$pSM3dZIRXDX9qX2R94tF1ehbH6HcKGwApQJDwl3NmC8bvmlIbmqrK', true);
+
+INSERT INTO users (username, password, enabled)
+
+VALUES ('user', '$2a$10$XTYrfRQzPUDsJ.smAKwRougTNtZ80Ak267mGWnaYYQMZG9KaZD2jy', true);
+
+INSERT INTO authorities (username, authority)
+
+VALUES ('user', 'ROLE_USER');
+
+INSERT INTO authorities (username, authority)
+
+VALUES ('admin', 'ROLE_ADMIN');
